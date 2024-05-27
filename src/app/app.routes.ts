ec5c32928df85ec3f49473c0b3d0c65114940fb1
Bittu <bittu1040@gmail.com>
2024-05-27 16:09:11 +0530

    image added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UploadSnippetsComponent } from './components/upload-snippets/upload-snippets.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 export const routes: Routes = [
     {path: "", redirectTo:"/home", pathMatch: "full"},
     {path: "home", component: HomeComponent},
-    {path: "upload-snippets", component: UploadSnippetsComponent}
+    {path: "upload-snippets", component: UploadSnippetsComponent},
+    {path: "login", component: LoginComponent},
+    {path: "signup", component: SignupComponent}
 ];
