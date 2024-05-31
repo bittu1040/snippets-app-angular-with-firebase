1182bc19a18af04555a1a471b789a5372272e2a6
Bittu <bittu1040@gmail.com>
2024-05-31 07:36:58 +0530

    error handing

---
+++ src/app/models/image.model.ts
@@ -0,0 +1,8 @@
+export interface Image {
+    id: string;
+    author: string;
+    width: number;
+    height: number;
+    url: string;
+    download_url: string;
+  }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { Image } from '../models/image.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,25 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
 
-  getImage(){
-    return this.http.get("https://picsum.photos/v2/list");
+  getImage():Observable<Image[]>{
+    return this.http.get<Image[]>("https://picsum.photos/v2/list").pipe(
+      catchError(this.handleError)
+    )
+  }
+
+
+  // simulate client side and server side error (TODO)
+
+
+  private handleError(error: HttpErrorResponse){
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(() => errorMessage)
   }
 }
