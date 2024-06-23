257fbbe38f7c7d550c73cd1cbfd6f1f7b36f9c1e
Bittu <bittu1040@gmail.com>
2024-06-23 09:50:30 +0530

    login error message

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FirebaseAuthService } from '../../services/firebase-auth.service';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -14,6 +15,7 @@ export class LoginComponent implements OnInit {
 
 
   loginForm: FormGroup = new FormGroup({});
+  loginError: string = '';
   constructor(private firebaseAuth: FirebaseAuthService, private router: Router, private fb: FormBuilder) { }  
 
   ngOnInit(){
@@ -25,19 +27,39 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     if (this.loginForm.valid) {
-      console.log('Email:', this.loginForm.value.email);
-      console.log('Password:', this.loginForm.value.password);
-      // Simulate login logic (replace with actual authentication)
+      console.log('Login form:', this.loginForm.value);
+
       this.firebaseAuth.login(this.loginForm.value.email, this.loginForm.value.password)
       .then((data) => {
-        console.log("login success", data)
+        console.log("login success", data);
+        this.loginError= '';
         this.router.navigate(['/home']);
       })
       .catch(error => {
-        console.log(error)
-      });
-      // Reset form after successful submission (optional)
-      this.loginForm.reset();
+        console.log(error.code)
+        this.loginError= this.getErrorMessage(error.code);
+      });   
+    }
+  }
+
+  getErrorMessage(errorCode: string): string {
+    switch (errorCode) {
+      case 'auth/invalid-email':
+        return 'Invalid email address format.';
+      case 'auth/user-disabled':
+        return 'This user has been disabled.';
+      case 'auth/user-not-found':
+        return 'User not found.';
+      case 'auth/wrong-password':
+        return 'Incorrect password.';
+      case 'auth/email-already-in-use':
+        return 'This email is already in use.';
+      case 'auth/weak-password':
+        return 'The password is too weak.';
+      case 'auth/invalid-credential':
+        return 'Invalid credentials.';
+      default:
+        return 'An unknown error occurred. Please try again.';
     }
   }
 }
