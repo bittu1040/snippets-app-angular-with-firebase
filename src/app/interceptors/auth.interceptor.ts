517d89c824015ade6d7ceecdb0a1cc2610d1e570
Bittu <bittu1040@gmail.com>
2024-06-27 23:58:32 +0530

    inject service

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,7 +1,10 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { FirebaseAuthService } from '../services/firebase-auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   // const authToken = localStorage.getItem('authToken');
+  const token= inject(FirebaseAuthService)
   const authToken= "Your auth token";
   const authReq = req.clone({
     setHeaders: {
