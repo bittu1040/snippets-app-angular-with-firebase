4feec591d2a55cf5248042a33e69bbf0efce9a63
Bittu <bittu1040@gmail.com>
2024-07-04 06:53:43 +0530

    @for control flow for displaying issues

--- src/app/components/issue-list/issue-list.component.ts
+++ src/app/components/issue-list/issue-list.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { Issue, Issues } from '../../shared/issues';
 
 @Component({
   selector: 'app-issue-list',
@@ -11,6 +12,14 @@ import { MatDividerModule } from '@angular/material/divider';
   templateUrl: './issue-list.component.html',
   styleUrl: './issue-list.component.scss'
 })
-export class IssueListComponent {
+export class IssueListComponent implements OnInit{
+
+  features: Issue[] = [];
+  bugs: Issue[] = [];
+  ngOnInit(): void {
+    this.features = Issues.filter((issue) => issue.issueType === "Feature");
+    this.bugs = Issues.filter((issue) => issue.issueType === "Bug");
+  }
+  colors = ["Red", "Blue", "White"];
 
 }
---
+++ src/app/shared/issues.ts
@@ -0,0 +1,136 @@
+export interface Issue{
+    userName: string;
+    issueType: string;
+    issue: string;
+    issueDescription: string;
+}
+
+
+export const Issues: Issue[] = [
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'Upload new image - this will be available for logged-in users.',
+        issueDescription: 'Upload new image - this will be available for logged-in users.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'Logged-in users can like/dislike images.',
+        issueDescription: 'Logged-in users can like/dislike images.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'Download image.',
+        issueDescription: 'Download image.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'Enlarge image on click.',
+        issueDescription: 'Enlarge image on click.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'Simple loader for API call until the response comes.',
+        issueDescription: 'Simple loader for API call until the response comes.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'Spinner for image loading time.',
+        issueDescription: 'Spinner for image loading time.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'Login/signup feature (JWT token with Firebase).',
+        issueDescription: 'Login/signup feature (JWT token with Firebase).'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'Route guards feature.',
+        issueDescription: 'Route guards feature.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'Custom validator for password and confirm password matching: in signup page.',
+        issueDescription: 'Custom validator for password and confirm password matching: in signup page.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'Interceptor for common error handling.',
+        issueDescription: 'Interceptor for common error handling.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'Unit testing.',
+        issueDescription: 'Unit testing.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'Detect if user is at the top or bottom of the page using HostListener (scroll event).',
+        issueDescription: 'Detect if user is at the top or bottom of the page using HostListener (scroll event).'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'Print username at the top after successful login.',
+        issueDescription: 'Print username at the top after successful login.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'Simple HTTP interceptor for sending auth token (try with all different cases).',
+        issueDescription: 'Simple HTTP interceptor for sending auth token (try with all different cases).'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Feature',
+        issue: 'When you click on any issues, it should show the description of the issue in a tooltip (if description available).',
+        issueDescription: 'When you click on any issues, it should show the description of the issue in a tooltip (if description available).'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Bug',
+        issue: 'Issue 1: Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        issueDescription: 'Issue 1: Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Bug',
+        issue: 'Issue 2: Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        issueDescription: 'Issue 2: Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Bug',
+        issue: 'Issue 3: Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        issueDescription: 'Issue 3: Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Bug',
+        issue: 'Issue 4: Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        issueDescription: 'Issue 4: Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Bug',
+        issue: 'Issue 5: Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        issueDescription: 'Issue 5: Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem.'
+    },
+    {
+        userName: 'Bittu',
+        issueType: 'Bug',
+        issue: 'Issue 6: Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        issueDescription: 'Issue 6: Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quidem.'
+    }
+];
